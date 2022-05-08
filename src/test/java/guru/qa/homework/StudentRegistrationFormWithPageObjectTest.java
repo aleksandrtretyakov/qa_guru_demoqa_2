814378e968ea8bd91e993253ebb23ddf3f79bf2d
aleksandrtretyakov <tretyakov.aa74+2@gmail.com>
2022-05-08 22:39:57 +0400

    added hw10 + improvements

--- src/test/java/guru/qa/homework/StudentRegistrationFormWithPageObjectTest.java
+++ src/test/java/guru/qa/homework/StudentRegistrationFormWithPageObjectTest.java
@@ -1,21 +1,17 @@
 package guru.qa.homework;
 
-import com.codeborne.selenide.Configuration;
 import guru.qa.pages.RegistrationFormPage;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
 import java.io.File;
 
-public class StudentRegistrationFormWithPageObjectTest {
+import static io.qameta.allure.Allure.step;
+
+@Tag("demoqa")
+public class StudentRegistrationFormWithPageObjectTest extends TestBase {
 
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
-
-    @BeforeAll
-    static void setUpTest() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-    }
 
     String firstName = "Aleksandr";
     String lastName = "Tretyakov";
@@ -33,33 +29,41 @@ public class StudentRegistrationFormWithPageObjectTest {
     @Test
     void fillFormTest() {
 
-        registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .chooseGenderMale("Male")
-                .setPhoneNumber(phoneNumber)
-                .chooseDateOfSomething(month, year, date)
-                .setSubject(subject)
-                .chooseHobby("Sports")
-                .uploadFile(file)
-                .addressInput(currentAddress)
-                .setState(state)
-                .setCity(city)
-                .clickOnSubmit();
+        step("Open student registration form", () -> {
+            registrationFormPage.openPage();
+        });
+
+        step("Fill student registration form", () -> {
+            registrationFormPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .chooseGenderMale("Male")
+                    .setPhoneNumber(phoneNumber)
+                    .chooseDateOfSomething(month, year, date)
+                    .setSubject(subject)
+                    .chooseHobby("Sports")
+                    .uploadFile(file)
+                    .addressInput(currentAddress)
+                    .setState(state)
+                    .setCity(city)
+                    .clickOnSubmit();
+        });
+
 
 //                Asserts
-        registrationFormPage.resultFormNameCheck("Thanks for submitting the form")
-                .resultFormDataCheck("Student Name", "Aleksandr Tretyakov")
-                .resultFormDataCheck("Student Email", "aleks@tret.com")
-                .resultFormDataCheck("Gender", "Male")
-                .resultFormDataCheck("Mobile", "8123456789")
-                .resultFormDataCheck("Date of Birth", "04 March,2000")
-                .resultFormDataCheck("Subjects", "Maths")
-                .resultFormDataCheck("Hobbies", "Sports")
-                .resultFormDataCheck("Picture", "qr-code.png")
-                .resultFormDataCheck("Address", "Street #1")
-                .resultFormDataCheck("State and City", "Haryana Karnal");
+        step("Verify student registration form", () -> {
+            registrationFormPage.resultFormNameCheck("Thanks for submitting the form")
+                    .resultFormDataCheck("Student Name", "Aleksandr Tretyakov")
+                    .resultFormDataCheck("Student Email", "aleks@tret.com")
+                    .resultFormDataCheck("Gender", "Male")
+                    .resultFormDataCheck("Mobile", "8123456789")
+                    .resultFormDataCheck("Date of Birth", "04 March,2000")
+                    .resultFormDataCheck("Subjects", "Maths")
+                    .resultFormDataCheck("Hobbies", "Sports")
+                    .resultFormDataCheck("Picture", "qr-code.png")
+                    .resultFormDataCheck("Address", "Street #1")
+                    .resultFormDataCheck("State and City", "Haryana Karnal");
+        });
 
     }
 }
