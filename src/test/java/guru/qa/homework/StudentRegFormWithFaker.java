f071ab98a5da1e7cf7a0feb85fbae49630004ce5
aleksandrtretyakov <tretyakov.aa74+2@gmail.com>
2022-05-09 22:57:23 +0400

    added smth

--- src/test/java/guru/qa/homework/StudentRegFormWithFaker.java
+++ src/test/java/guru/qa/homework/StudentRegFormWithFaker.java
@@ -4,7 +4,9 @@ import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
