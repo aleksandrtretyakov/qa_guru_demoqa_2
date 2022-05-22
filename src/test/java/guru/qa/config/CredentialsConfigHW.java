1187da41a6ef4351d3fea56f9a16f28762ae9e87
aleksandrtretyakov <tretyakov.aa74+2@gmail.com>
2022-05-22 15:34:41 +0400

    added owner hw

---
+++ src/test/java/guru/qa/config/CredentialsConfigHW.java
@@ -0,0 +1,12 @@
+package guru.qa.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:config/credentials.properties")
+public interface CredentialsConfigHW extends Config {
+    String user();
+    String userPassword();
+    String baseUrl();
+    String browserSize();
+
+}
